8fbb68b5960c9c33c6c7eef92370e38ee248525c
Emantella <56229347+Emantella@users.noreply.github.com>
2021-07-21 19:27:43 -0700

    Add files via upload

---
+++ LT.bat
@@ -0,0 +1,15 @@
+@echo off
+echo color green
+color 02
+echo 40/100
+timeout /nobreak 2
+echo .
+timeout /nobreak 1
+echo .
+echo clearing cmd
+echo 70/100
+Timeout /nobreak 2
+cls
+echo Done 100/100
+timeout 5
+cls
